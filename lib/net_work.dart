fe18267e94ae9e2178420d614591a619d57d41a4
JaehyunYoo <nike361@naver.com>
2024-01-16 18:54:26 +0900

    test

--- lib/net_work.dart
+++ lib/net_work.dart
@@ -7,7 +7,7 @@ class NetWorkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'NEt Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
