7abded0b926d374bf057a709d25d1c5401dc9a05
JaehyunYoo <nike361@naver.com>
2024-01-17 11:24:07 +0900

    굿 테스ㅌ스

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,8 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:test_app/main.dart';
 
 void main() {
-  testWidgets('Counter111111 increments smoke test',
-      (WidgetTester tester) async {
+  testWidgets('222222 increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
